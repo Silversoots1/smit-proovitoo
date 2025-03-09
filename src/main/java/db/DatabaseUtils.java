b3b4c4d06b5e74126eb31d11b10409c54fcec8f7
Silversoots1 <silversoots1@gmail.com>
2025-03-09 16:43:45 +0200

    Added db info to conf

--- src/main/java/db/DatabaseUtils.java
+++ src/main/java/db/DatabaseUtils.java
@@ -1,16 +1,36 @@
 package db;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseUtils {
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/mysql";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "1234";
+    private static final String PROPERTIES_FILE = "db.properties";
+    private static String dbUrl;
+    private static String dbUser;
+    private static String dbPassword;
+
+    static {
+        try (InputStream input = DatabaseUtils.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
+            Properties properties = new Properties();
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + PROPERTIES_FILE);
+            }
+            properties.load(input);
+            dbUrl = properties.getProperty("db.url");
+            dbUser = properties.getProperty("db.user");
+            dbPassword = properties.getProperty("db.password");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
 }
